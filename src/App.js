4d7f51c5f9182502b17ae2c47659681bccec3312
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2021-09-23 17:51:49 +0200

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-
-import './App.css';
+import "./App.css";
+import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
+import LoginView from "./Components/LoginView";
+import RegisterView from "./Components/RegisterView";
+import { PageContainer } from "./PageContainer";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <PageContainer>
+        <Switch>
+          <Route exact path="/Login">
+            <LoginView />
+          </Route>
+          <Route exact path="/register">
+            <RegisterView />
+          </Route>
+          <Route exact path="/dashboard">
+            <h1>Welcome to Dashboard Page! 🎊</h1>
+          </Route>
+          <h1>No such page 😭</h1>
+        </Switch>
+        {/* Temporary links to pages under constraction: */}
+        <Link to="/Login">Login</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/dashboard">Dashboard</Link>
+      </PageContainer>
+    </BrowserRouter>
   );
 }
 
